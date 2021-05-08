72d83c4110fe7cafe6b81bcbee41c177d291b744
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-05-08 08:49:12 +0200

    feat: added navigation to the client using react-router-dom

---
+++ client/src/pages/PageNotFound.tsx
@@ -0,0 +1,12 @@
+import './PageNotFound.css';
+import React from 'react';
+
+const PageNotFound: React.FC = () => {
+  return (
+    <div>
+      <h1>Ops... the page you are after, we have not found...</h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
