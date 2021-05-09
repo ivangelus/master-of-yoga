22336d00f8065911bc324d2325da8a5036c634af
ivan_gelo <geloivan21@gmail.com>
2021-05-09 17:06:42 +0200

    feat: userCard component first mock

--- client/src/containers/UserCard.tsx
+++ client/src/containers/UserCard.tsx
@@ -1,10 +1,45 @@
 import './UserCard.css';
 import React from 'react';
+import { UserDTO } from '../interfaces/UserDTO';
 
 const UserCard: React.FC = () => {
+  const mockUser: UserDTO = {
+    firstName: 'Luke',
+    lastName: 'SkyWalker',
+    email: 'luke@email.com',
+    password: '1234',
+    lastEntry: 'Today',
+    consecutiveDays: 5,
+  };
+  console.log(mockUser); // only so that linter doesn't throw an error
+
   return (
     <div className="user-card-container">
-      <h1>UserCard</h1>
+      <div className="user-card-left">
+        <div className="profile-image"></div>
+        <button className="edit-profile">Edit Profile</button>
+      </div>
+      <div className="user-card-right">
+        <div className="title-user-name">
+          <div className="user-name">
+            {mockUser.firstName} {mockUser.lastName}
+          </div>
+        </div>
+        <div className="card-content">
+          <div className="email-container">
+            <div className="content-description">Email</div>
+            <div className="content-text">{mockUser.email}</div>
+          </div>
+          <div className="last-entry-container">
+            <div className="content-description">Last Entry</div>
+            <div className="content-text">{mockUser.lastEntry}</div>
+          </div>
+          <div className="days-container">
+            <div className="content-description">Streak</div>
+            <div className="content-text">{mockUser.consecutiveDays} days!</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
