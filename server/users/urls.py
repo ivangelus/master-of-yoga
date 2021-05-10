0e65bc61632a7a38915f731ebaf8074621370b5d
Stefan Sarmir <stefansarmir@gmail.com>
2021-05-10 20:43:11 +0200

    refactor: adds google user to database when registering with google

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
 from knox import views as knox_views
-from .views import LoginView, VerifyView, RegisterView
+from .views import VerifyView, RegisterView
 
 urlpatterns = [
-  path('api/users/login', LoginView.as_view()),
   path('api/users/verify', VerifyView.as_view()),
   path('api/users', RegisterView.as_view()),
 ]
--- server/users/views.py
+++ server/users/views.py
@@ -4,14 +4,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from server import firebase
 import json
-
-class LoginView(APIView):
-  def post(self, request):
-    parsedRequestBody = json.loads(request.body)
-    loginResponse = firebase.logIn(parsedRequestBody['email'], parsedRequestBody['password'])
-    print(loginResponse)
-    return Response(loginResponse)
-
 class VerifyView(APIView):
   def post(self, request):
     parsedRequestBody = json.loads(request.body)
