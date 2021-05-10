f45b2cb525d33fcb17a8eb2344d21ba905fa8dc6
ignaraph <ignacioraphael@hotmail.com>
2021-05-10 09:15:05 +0200

    feat(posevalidation page): added initial logic for the pose validation page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import About from './pages/About';
 import LandingPage from './pages/LandingPage';
 import PageNotFound from './pages/PageNotFound';
+import PoseValidation from './pages/PoseValidation';
 
 function App(): ReactElement {
   return (
@@ -12,6 +13,7 @@ function App(): ReactElement {
       <Switch>
         <Route path="/" component={LandingPage} exact />
         <Route path="/about" component={About} />
+        <Route path="/pose" component={PoseValidation} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
