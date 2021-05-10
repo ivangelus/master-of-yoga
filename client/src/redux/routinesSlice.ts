4817fb88a8acbfcbfb180b82673b7d485a5525ad
ivan_gelo <geloivan21@gmail.com>
2021-05-10 19:43:20 +0200

    feat: implementing routines slicer, and fetching and setting the data on dashboard page

---
+++ client/src/redux/routinesSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from './store';
+
+import { BaseRoutinesDTO } from '../interfaces/RoutineDTO';
+
+const initialState: BaseRoutinesDTO = {}
+
+export const routinesSlice = createSlice({
+  name: 'routines',
+  initialState,
+  reducers: {
+    updateRoutines: (state, action: PayloadAction<BaseRoutinesDTO>) => {
+      return { ...state, ...action.payload };
+    },
+  },
+});
+
+export const { updateRoutines } = routinesSlice.actions;
+export const selectRoutines = (state: RootState) => state.routines;
+export default routinesSlice.reducer;
