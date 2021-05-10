9c2892d6e8862c71e4f8c11152c1017c99e90a8b
ivan_gelo <geloivan21@gmail.com>
2021-05-10 11:20:58 +0200

    style: checking what is the nature of commiting on a branch that has closed pull request

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -17,7 +17,7 @@ const Dashboard: React.FC = () => {
           <Tracks title="Advanced" />
         </div>
         <div className="dashboard-content-down">
-          <AttendanceGraph />
+          {/* <AttendanceGraph /> */}
           <Tips />
         </div>
       </div>
