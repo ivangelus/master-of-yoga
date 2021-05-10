b2f02cd8edee25aadd4026f9ae35267eaf8b2bf7
ivan_gelo <geloivan21@gmail.com>
2021-05-10 09:13:11 +0200

    feat: started working on tips and attendance graph components, dashboard is incomplete

--- client/src/containers/Tips.tsx
+++ client/src/containers/Tips.tsx
@@ -4,7 +4,17 @@ import React from 'react';
 const Tips: React.FC = () => {
   return (
     <div className="tips-container">
-      <h1>Tips</h1>
+      <h3>Quote of the day</h3>
+      <div className="content-container">
+        <p className="quote-content">
+          Exercises are like prose, whereas yoga is the poetry of movements.
+          Once you understand the grammar of yoga; you can write your poetry of
+          movements.
+        </p>
+        <p className="quote-author">
+          ― Amit Ray, Yoga and Vipassana: An Integrated Life Style
+        </p>
+      </div>
     </div>
   );
 };
