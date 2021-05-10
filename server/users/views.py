a4a0f8cea4a0d1ea385e467fab2f64a9e8942e3b
Stefan Sarmir <stefansarmir@gmail.com>
2021-05-10 10:38:53 +0200

    refactor: deleted serializers and models related to postgreSQL

--- server/users/views.py
+++ server/users/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import User
 from server import firebase
 import json
 
