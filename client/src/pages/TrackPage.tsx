2e8329577e37645bd2875a4919b9cb603ed081c9
ivan_gelo <geloivan21@gmail.com>
2021-05-11 08:23:47 +0200

    feat: start work on having a dynamic route param to use in the track page

---
+++ client/src/pages/TrackPage.tsx
@@ -0,0 +1,15 @@
+import './TrackPage.css';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const TrackPage: React.FC = () => {
+  // const { level } = useParams();
+  return (
+    <div>
+      <h1>TrackPage</h1>
+      {/* <p>{level}</p> */}
+    </div>
+  );
+};
+
+export default TrackPage;
