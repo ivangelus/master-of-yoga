a1cbd33e514b414381c7adfd4ab6bdb953fc61e1
ignaraph <ignacioraphael@hotmail.com>
2021-05-13 11:22:57 +0200

    Configuring the window.env file to run app

--- client/public/env.js
+++ client/public/env.js
@@ -1,3 +1,4 @@
 window.env = {
-  "FIREBASE_APIKEY": "AIzaSyAWsVz3Lk4OZikWaCjkDrcpSbIRo_PpBXg"
+  "FIREBASE_APIKEY": "AIzaSyAWsVz3Lk4OZikWaCjkDrcpSbIRo_PpBXg",
+  "SERVER_ADDRESS": "http://localhost:8000"
 };
