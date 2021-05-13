10380c079e4234f6c2970b4f12f2e61b3d60ee0a
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-05-13 11:29:00 +0200

    feat: hooked up classifier loading to localStorage from user dashboard

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import modalReducer from './modalSlice';
 import usersReducer from './usersSlice';
 import routinesReducer from './routinesSlice';
+import classifierReducer from './classifierSlice';
 
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
     users: usersReducer,
     routines: routinesReducer,
+    classifier: classifierReducer,
   },
 });
 
