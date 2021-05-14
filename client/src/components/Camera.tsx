4caa29efc2c6b3c9b2066de4293327bd718e5559
Stefan Sarmir <stefansarmir@gmail.com>
2021-05-14 18:40:56 +0200

    refactor: refactored camera file. screen slightly more performant, code cleaner

--- client/src/components/Camera.tsx
+++ client/src/components/Camera.tsx
@@ -1,150 +1,65 @@
-import {
-  drawKeypoints,
-  drawSkeleton,
-  setColor,
-} from '../utilities/drawingUtilities';
-import { PoseNetOutputDTO } from '../interfaces/PoseNetOutputDTO';
-import poseEvaluator from '../utilities/poseEvaluator';
+import { initClassifier, initPoseNet } from '../utilities/initModels';
 import { Classifier } from '../interfaces/ClassifierDTO';
-import * as poseNet from '@tensorflow-models/posenet';
-import React, { useRef, useEffect } from 'react';
-import normalizer from '../utilities/normalizer';
+import React, { useRef, useEffect, useState } from 'react';
+import { detect } from '../utilities/cameraHelpers';
 import { useAppSelector } from '../redux/hooks';
 import type { MutableRefObject } from 'react';
 import { RootState } from '../redux/store';
 import * as tf from '@tensorflow/tfjs';
 import Webcam from 'react-webcam';
 import './Camera.css';
-
 interface Props {
   poseName: string;
 }
 
 const Camera: React.FC<Props> = ({ poseName }: Props): React.ReactElement => {
+  const [executeDetection, setExecuteDetection] = useState(false);
   const webcamRef: MutableRefObject<any> = useRef(null);
   const canvasRef: MutableRefObject<any> = useRef(null);
   const classifier: Classifier = useAppSelector(
     (state: RootState) => state.classifier
   );
-  const classifierKey = classifier.storageKey;
   const classifierLabels: string[] = classifier.labels;
-
-  const drawCanvas = (
-    pose: PoseNetOutputDTO,
-    video: { height: number; width: number },
-    videoWidth: number,
-    videoHeight: number,
-    canvas: MutableRefObject<any>,
-    evaluatedPose: { pose: string; confidence: number },
-    poseName: string
-  ) => {
-    if (canvas.current) {
-      const ctx = canvas.current.getContext('2d');
-      canvas.current.width = videoWidth;
-      canvas.current.height = videoHeight;
-      if (poseName === evaluatedPose.pose) setColor('green');
-      else setColor('red');
-      // console.log(pose);
-      drawKeypoints(pose['keypoints'], 0.7, ctx);
-      drawSkeleton(pose['keypoints'], 0.7, ctx);
-    }
-  };
-
-  const detect = async (net: any, classifierModel: tf.LayersModel) => {
-    if (
-      typeof webcamRef.current !== 'undefined' &&
-      webcamRef.current !== null &&
-      webcamRef.current.video.readyState === 4
-    ) {
-      //Get video properties
-      const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
-
-      //Set video width
-      webcamRef.current.video.width = videoWidth;
-      webcamRef.current.video.height = videoHeight;
-
-      //Make Detections
-      const pose: any = await net.estimateSinglePose(video);
-      let evaluatedPose: { pose: string; confidence: number };
-      // const label = 'heroPose';
-      // console.log(pose.keypoints);
-      if (pose.score >= 0.5) {
-        const resultArr: number[] = await normalizer(
-          pose.keypoints,
-          window.innerWidth / 2,
-          window.innerHeight
-        );
-        // savedData.push({'xs': resultArr, 'ys': label});
-        // localStorage.setItem('heroPose', JSON.stringify(savedData))
-        const predictionResult = classifierModel.predict(
-          tf.tensor(resultArr, [1, 34])
-        ) as tf.Tensor;
-        const predictionResultArray: any = await predictionResult.array();
-        evaluatedPose = await poseEvaluator(
-          classifierLabels,
-          predictionResultArray
-        );
-        // console.log(evaluatedPose);
-      } else {
-        evaluatedPose = { pose: 'none', confidence: 0 };
-        // console.log('low confidence score');
-      }
-      drawCanvas(
-        pose,
-        video,
-        videoWidth,
-        videoHeight,
-        canvasRef,
-        evaluatedPose,
-        poseName
-      );
-    }
-  };
+  const classifierKey = classifier.storageKey;
+  let poseNetModel: any = undefined;
+  let classifierModel: tf.LayersModel | undefined = undefined;
+  let interval: NodeJS.Timer;
 
   useEffect(() => {
-    let interval: NodeJS.Timeout;
-    (async () => {
-      const net = await poseNet.load({
-        architecture: 'ResNet50',
-        outputStride: 32,
-        inputResolution: { width: 250, height: 250 },
-        quantBytes: 2,
-      });
-      const classifierModel = await tf.loadLayersModel(
-        `localstorage://${classifierKey}`
-      );
-      interval = setInterval(() => {
-        detect(net, classifierModel);
-      }, 250);
-    })();
-    return () => clearInterval(interval);
-  }, [poseName]);
-
-  // const runPosnet = async () => {
-  //   const net = await posenet.load({
-  //     architecture: 'ResNet50',
-  //     outputStride: 32,
-  //     inputResolution: { width: 257, height: 200 },
-  //     quantBytes: 4,
-  //   });
-  //   setInterval(() => {
-  //     detect(net);
-  //   }, 250);
-  // };
+    async function init() {
+      poseNetModel = await initPoseNet();
+      classifierModel = await initClassifier(classifierKey);
+      setExecuteDetection(true);
+    }
+    init();
+    interval = setInterval(
+      () =>
+        detect(
+          poseNetModel,
+          classifierModel,
+          webcamRef,
+          classifierLabels,
+          canvasRef,
+          poseName
+        ),
+      300
+    );
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   const videoConstraints = {
     width: window.innerWidth / 2,
     height: window.innerHeight,
     facingMode: 'user',
   };
+
   return (
     <div>
       <div className="webcam__container">
         <Webcam
           audio={false}
-          // mirrored={true}
           className="webcam__screen"
           videoConstraints={videoConstraints}
           ref={webcamRef}
