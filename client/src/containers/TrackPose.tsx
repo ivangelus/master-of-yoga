e55bbada229628ad9f965f83b9234d226ec2dbae
ignaraph <ignacioraphael@hotmail.com>
2021-05-14 18:01:53 +0200

    feat(track page): created a range slider bar also created a component for the drop down instructions

--- client/src/containers/TrackPose.tsx
+++ client/src/containers/TrackPose.tsx
@@ -17,31 +17,36 @@ const TrackPose: React.FC<Props> = ({ routine, index }: Props) => {
   const [clicked1, setClicked1] = useState(false);
   const [clicked2, setClicked2] = useState(false);
   const [clicked3, setClicked3] = useState(false);
+  const [sliderValue, setSliderValue] = useState(50);
 
   const handleClick = (): void => {
     history.push(`/pose/${routine.level}/${index}`);
   };
 
-  function changeColor(num: number, state: boolean) {
-    if (num === 1) {
-      setColor1('#27ae60');
-      setColor2('#f5f6fa');
-      setColor3('#f5f6fa');
-      setClicked1(!state);
-      return;
-    } else if (num === 2) {
-      setColor1('#f5f6fa');
-      setColor2('#f1c40f');
-      setColor3('#f5f6fa');
-      setClicked2(!state);
-      return;
-    } else {
-      setColor1('#f5f6fa');
-      setColor2('#f5f6fa');
-      setColor3('#c0392b');
-      setClicked3(!state);
-    }
-  }
+  const handleChange = (event: any) => {
+    setSliderValue(event.target.value);
+  };
+
+  // function changeColor(num: number, state: boolean) {
+  //   if (num === 1) {
+  //     setColor1('#27ae60');
+  //     setColor2('#f5f6fa');
+  //     setColor3('#f5f6fa');
+  //     setClicked1(!state);
+  //     return;
+  //   } else if (num === 2) {
+  //     setColor1('#f5f6fa');
+  //     setColor2('#f1c40f');
+  //     setColor3('#f5f6fa');
+  //     setClicked2(!state);
+  //     return;
+  //   } else {
+  //     setColor1('#f5f6fa');
+  //     setColor2('#f5f6fa');
+  //     setColor3('#c0392b');
+  //     setClicked3(!state);
+  //   }
+  // }
   return (
     <div className="allTracks__container">
       <div className="track-pose-container">
@@ -65,7 +70,25 @@ const TrackPose: React.FC<Props> = ({ routine, index }: Props) => {
           </button>
         </div>
         <div className="levels__container">
-          <button
+          <div className="levels__container--labels">
+            <p>Hard</p>
+            <p>Intermediate</p>
+            <p>Beginner</p>
+          </div>
+          <div className="levels__container--slider-container">
+            <input
+              type="range"
+              min="0"
+              max="100"
+              value={sliderValue}
+              onChange={handleChange}
+              className="levels__container--rangeSlider"
+            />
+          </div>
+        </div>
+        {/* <div className="levels__container"> */}
+        {/* <input type="range" min="0" max="100" value={sliderValue} onChange={handleChange} className="range__slider"/> */}
+        {/* <button
             onClick={() => changeColor(1, clicked1)}
             style={{ backgroundColor: color1 }}
             className="levels__btn easy"
@@ -85,8 +108,8 @@ const TrackPose: React.FC<Props> = ({ routine, index }: Props) => {
             className="levels__btn hard"
           >
             <p className="btn__text">Hard</p>
-          </button>
-        </div>
+          </button> */}
+        {/* </div> */}
       </div>
     </div>
   );
