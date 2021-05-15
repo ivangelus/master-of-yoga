09bed04be5f715f21ec5c33e1a343b985b549b4b
ignaraph <ignacioraphael@hotmail.com>
2021-05-15 15:47:53 +0200

    style(track page): created "bubbles" to display difficulty levels information

--- client/src/containers/TrackPose.tsx
+++ client/src/containers/TrackPose.tsx
@@ -12,12 +12,12 @@ interface Props {
 const TrackPose: React.FC<Props> = ({ routine, index }: Props) => {
   const history = useHistory();
 
-  const [color1, setColor1] = useState('#f5f6fa');
-  const [color2, setColor2] = useState('#f5f6fa');
-  const [color3, setColor3] = useState('#f5f6fa');
-  const [clicked1, setClicked1] = useState(false);
-  const [clicked2, setClicked2] = useState(false);
-  const [clicked3, setClicked3] = useState(false);
+  // const [color1, setColor1] = useState('#f5f6fa');
+  // const [color2, setColor2] = useState('#f5f6fa');
+  // const [color3, setColor3] = useState('#f5f6fa');
+  // const [clicked1, setClicked1] = useState(false);
+  // const [clicked2, setClicked2] = useState(false);
+  // const [clicked3, setClicked3] = useState(false);
   const [sliderValue, setSliderValue] = useState(50);
   const [instructions, setInstructions] = useState(false);
 
@@ -32,35 +32,15 @@ const TrackPose: React.FC<Props> = ({ routine, index }: Props) => {
   const renderInstructions = () => {
     setTimeout(() => {
       setInstructions((instructions) => !instructions);
-    }, 1200);
+    }, 1000);
   };
 
   const removeInstructions = () => {
     setTimeout(() => {
       setInstructions((instructions) => !instructions);
-    }, 700);
+    }, 500);
   };
 
-  // function changeColor(num: number, state: boolean) {
-  //   if (num === 1) {
-  //     setColor1('#27ae60');
-  //     setColor2('#f5f6fa');
-  //     setColor3('#f5f6fa');
-  //     setClicked1(!state);
-  //     return;
-  //   } else if (num === 2) {
-  //     setColor1('#f5f6fa');
-  //     setColor2('#f1c40f');
-  //     setColor3('#f5f6fa');
-  //     setClicked2(!state);
-  //     return;
-  //   } else {
-  //     setColor1('#f5f6fa');
-  //     setColor2('#f5f6fa');
-  //     setColor3('#c0392b');
-  //     setClicked3(!state);
-  //   }
-  // }
   return (
     <div className="allTracks__container">
       <div className="track-pose-container">
@@ -85,48 +65,19 @@ const TrackPose: React.FC<Props> = ({ routine, index }: Props) => {
         </div>
         <div className="levels__container">
           <div className="levels__container__instructions">
-            {/* <button onClick={renderInstructions} className="levels__btn">
-              Toggle Instructions
-            </button> */}
             {instructions ? <Instructions /> : ''}
           </div>
-          <div className="levels__container--slider-container">
-            <input
-              onMouseEnter={renderInstructions}
-              onMouseLeave={removeInstructions}
-              type="range"
-              min="0"
-              max="100"
-              value={sliderValue}
-              onChange={handleChange}
-              className="levels__container--rangeSlider"
-            />
-          </div>
+          <input
+            onMouseEnter={renderInstructions}
+            onMouseLeave={removeInstructions}
+            type="range"
+            min="0"
+            max="100"
+            value={sliderValue}
+            onChange={handleChange}
+            className="levels__container--rangeSlider"
+          />
         </div>
-        {/* <div className="levels__container"> */}
-        {/* <input type="range" min="0" max="100" value={sliderValue} onChange={handleChange} className="range__slider"/> */}
-        {/* <button
-            onClick={() => changeColor(1, clicked1)}
-            style={{ backgroundColor: color1 }}
-            className="levels__btn easy"
-          >
-            <p className="btn__text">Easy</p>
-          </button>
-          <button
-            onClick={() => changeColor(2, clicked2)}
-            style={{ backgroundColor: color2 }}
-            className="levels__btn medium"
-          >
-            <p className="btn__text">Medium</p>
-          </button>
-          <button
-            onClick={() => changeColor(3, clicked3)}
-            style={{ backgroundColor: color3 }}
-            className="levels__btn hard"
-          >
-            <p className="btn__text">Hard</p>
-          </button> */}
-        {/* </div> */}
       </div>
     </div>
   );
