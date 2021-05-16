6689225f2b1667e408e6f280cb70d7b97524a10e
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-05-16 12:29:59 +0200

    feat: leverages multiple binary classification models to increase accuracy

--- client/src/components/Camera.tsx
+++ client/src/components/Camera.tsx
@@ -1,15 +1,13 @@
 import { initClassifier, initPoseNet } from '../utilities/initModels';
 import { setIntervalAsync } from 'set-interval-async/dynamic';
 import { clearIntervalAsync } from 'set-interval-async';
-import { Classifier } from '../interfaces/ClassifierDTO';
 import React, { useRef, useEffect } from 'react';
 import { detect } from '../utilities/cameraHelpers';
-import { useAppSelector } from '../redux/hooks';
 import type { MutableRefObject } from 'react';
-import { RootState } from '../redux/store';
 import * as tf from '@tensorflow/tfjs';
 import Webcam from 'react-webcam';
 import './Camera.css';
+
 interface Props {
   poseName: string;
   setPoseOK: any;
@@ -21,11 +19,6 @@ const Camera: React.FC<Props> = ({
 }: Props): React.ReactElement => {
   const webcamRef: MutableRefObject<any> = useRef(null);
   const canvasRef: MutableRefObject<any> = useRef(null);
-  const classifier: Classifier = useAppSelector(
-    (state: RootState) => state.classifier
-  );
-  const classifierLabels: string[] = classifier.labels;
-  const classifierKey = classifier.storageKey;
   let poseNetModel: any;
   let classifierModel: tf.LayersModel | undefined;
   let interval: any;
@@ -33,8 +26,7 @@ const Camera: React.FC<Props> = ({
   useEffect(() => {
     async function init() {
       if (poseNetModel === undefined) poseNetModel = await initPoseNet();
-      if (classifierModel === undefined)
-        classifierModel = await initClassifier(classifierKey);
+      classifierModel = await initClassifier(poseName);
       if (interval) await clearIntervalAsync(interval);
       interval = setIntervalAsync(
         async () =>
@@ -42,7 +34,6 @@ const Camera: React.FC<Props> = ({
             poseNetModel,
             classifierModel,
             webcamRef,
-            classifierLabels,
             canvasRef,
             poseName,
             setPoseOK
